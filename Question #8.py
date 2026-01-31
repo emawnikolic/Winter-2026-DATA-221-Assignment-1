8a26a51d851cf9a1e7ecd34ced77e6c03738a20c
ema <ema.w.nikolic@gmail.com>
2026-01-30 17:43:28 -0700

    finish question 8

--- Question #8.py
+++ Question #8.py
@@ -2,4 +2,17 @@
 # • Create a DataFrame using the provided column data.
 # • Add a new column derived from existing columns.
 # • Print the final DataFrame.
-#%%
+
+import pandas as pd
+
+data = {
+    "A": [1, 2, 2, 1],
+    "B": [3.1, 4.2, 1.5, 6.3],
+    "C": [800, 150, 400, 210]
+}
+
+df = pd.DataFrame(data)
+
+df["Total"] = df["A"] + df["B"] + df["C"]
+
+print(df)
