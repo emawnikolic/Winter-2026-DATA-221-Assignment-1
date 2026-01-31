43946daf1c41a56365961a1392d093706a01f130
ema <ema.w.nikolic@gmail.com>
2026-01-30 17:30:40 -0700

    finish question 6

--- Question #6.py
+++ Question #6.py
@@ -3,3 +3,19 @@
 # • Each value is the percentage of elements in the list that are less than or equal to that key.
 # The resulting dictionary should be sorted by key before being returned.
 
+def distribution_analysis(nums):
+    result = {}
+    n = len(nums)
+
+    for value in set(nums):
+        count = 0
+        for num in nums:
+            if num <= value:
+                count += 1
+        result[value] = (count / n) * 100
+
+    sorted_result = dict(sorted(result.items()))
+    return sorted_result
+
+numbers = [3, 1, 2, 3, 4, 2]
+print(distribution_analysis(numbers))
